7ecae1699eea54218254396d8297c2c7e60dfb66
Mr_Faton <firefly90@inbox.ru>
2015-05-03 11:16:17 +0300

    update from work

---
+++ src/com/mr_faton/TestApp.java
@@ -0,0 +1,10 @@
+package com.mr_faton;
+
+/**
+ * Created by root on 03.05.2015.
+ */
+public class TestApp {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
